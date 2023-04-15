49332dde5e65cd1ecb2b9869be5879bf717427b6
Kenechukwu Frank <kenisank1@gmail.com>
2023-04-15 19:17:00 +0100

    hh

--- ClinicManagement/Core/ViewModel/PatientDetailViewModel.cs
+++ ClinicManagement/Core/ViewModel/PatientDetailViewModel.cs
@@ -5,7 +5,7 @@ namespace ClinicManagement.Core.ViewModel
 {
     public class PatientDetailViewModel
     {
-        public Patient Patient { get; set; }
+        public virtual Patient Patient { get; set; }
         public IEnumerable<Appointment> Appointments { get; set; }
         public IEnumerable<Attendance> Attendances { get; set; }
         public int CountAppointments { get; set; }
